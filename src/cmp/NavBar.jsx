0504d8d97200ea8b2a246c65328d84ba897deb27
Or Ben Yair <orbenyair@Or-Ben-Yair-VMFJKXVVFV.local>
2025-05-24 11:07:37 +0300

    Adding structure for the css and + some progress in the navbar

--- src/cmp/NavBar.jsx
+++ src/cmp/NavBar.jsx
@@ -3,15 +3,23 @@ import { IconsSvg } from "./IconSvg"
 export function NavBar() {
 
     return (
-        <nav className="nav-bar">
-            <ul>
+        <nav className="nav-bar-container">
+            <ul className="nav-bar">
                 <li><a href="#">
                     <IconsSvg svgName={"home"} />
                 </a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
+                <li><a href="#">
+                    <IconsSvg svgName={"search"} />    
+                </a></li>
+                <li><a href="#">
+                    <IconsSvg svgName={"add"} />
+                </a></li>
+                <li><a href="#">
+                    <IconsSvg svgName={"bell"} />
+                </a></li>
+                <li><a href="#">
+                    <IconsSvg svgName={"user"} />
+                </a></li>
             </ul>
         </nav>
     )
