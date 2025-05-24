540dfb35bf612fb6a1f4e192e4dc159c4dc30a6d
Or Ben Yair <orbenyair@Or-Ben-Yair-VMFJKXVVFV.local>
2025-05-24 10:51:33 +0300

    Initial commit

---
+++ src/cmp/NavBar.jsx
@@ -0,0 +1,15 @@
+
+export function NavBar() {
+
+    return (
+        <nav className="nav-bar">
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+    )
+}
