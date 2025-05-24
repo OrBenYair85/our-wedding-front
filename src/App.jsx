38afb411b8db5b6cd8c3e4982aabc589aa08bd82
Or Ben Yair <orbenyair@Or-Ben-Yair-VMFJKXVVFV.local>
2025-05-24 11:03:37 +0300

    Adding the navbar to the home page

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { NavBar } from "./cmp/NavBar";
+
+export function App () {
+    return (
+        <>
+            <NavBar />
+        </>
+    )
+}
--- src/cmp/NavBar.jsx
+++ src/cmp/NavBar.jsx
@@ -1,10 +1,13 @@
+import { IconsSvg } from "./IconSvg"
 
 export function NavBar() {
 
     return (
         <nav className="nav-bar">
             <ul>
-                <li><a href=""></a></li>
+                <li><a href="#">
+                    <IconsSvg svgName={"home"} />
+                </a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
                 <li><a href=""></a></li>
