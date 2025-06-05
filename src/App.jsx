c72ef8d4331a35749ef4ad79d456eb022152a21d
Or Ben Yair <orthe85@gmail.com>
2025-06-05 20:43:05 +0300

    end of working on the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { NavBar } from "./cmp/NavBar";
+import { Layout } from './cmp/Layout';
 import { Home } from './pages/Home';
 import { Search } from './pages/Search';
 import { AddPhoto } from './pages/AddPhoto';
 import { Notification } from './pages/Notification';
 import { ProfilePage } from './pages/ProfilePage';
-import { AppHeader } from './cmp/AppHeader';
 
 export function App() {
   return (
     <Router>
-      <AppHeader />
-      <NavBar />
-      <main>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path="/search" element={<Search />} />
-            <Route path='/add' element={<AddPhoto />} />
-            <Route path='/notifications' element={<Notification />} />
-            <Route path='/profile' element={<ProfilePage />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="add" element={<AddPhoto />} />
+          <Route path="notifications" element={<Notification />} />
+          <Route path="profile" element={<ProfilePage />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
